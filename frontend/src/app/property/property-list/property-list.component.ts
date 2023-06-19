8f27f87b9b638935432abbaae74e6c15149ac9ca
Austin Tormasi <atormasi@gmail.com>
2023-06-19 16:15:44 -0400

    Day1

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -3,8 +3,59 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent {
+  Properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'Erose Villa',
+      Type: 'House',
+      Price: 14000,
+    },
+    {
+      Id: 3,
+      Name: 'Tuttles grove ',
+      Type: 'Apartament',
+      Price: 1205,
+    },
 
+    {
+      Id: 4,
+      Name: 'Tormasi Townhome',
+      Type: 'House',
+      Price: 450000,
+    },
+    {
+      Id: 5,
+      Name: 'Millrun Estate',
+      Type: 'House',
+      Price: 1000000,
+    },
+
+    {
+      Id: 6,
+      Name: 'Sawmill Apartments',
+      Type: 'Apartament',
+      Price: 1500,
+    },
+    {
+      Id: 7,
+      Name: 'Dublin Homes',
+      Type: 'Apartment',
+      Price: 12000,
+    },
+    {
+      Id: 7,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+  ];
 }
